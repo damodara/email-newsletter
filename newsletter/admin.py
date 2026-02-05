b09d2c8f5147f46b458b2e0f58c7a1eff467cafa
damodara <jay108@mail.ru>
2026-02-05 22:02:13 +0300

    решил проблему с БД и изменил шаблон, создал суперпользователя

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from newsletter.models import Message, Subscriber
+
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('first_name', 'last_name', 'email')
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'text')
+    search_fields = ('subject', 'text')
+    list_filter = ('subject', 'text')
+
--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,43 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+class Subscriber(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name="Имя")
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+    surname = models.CharField(max_length=100, verbose_name="Отчество", blank=True, null=True)
+    email = models.EmailField(unique=True, verbose_name="E-mail")
+    comment = models.TextField(verbose_name="Комментарий", blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="Дата создания")
+
+    class Meta:
+        verbose_name = "Подписчик"
+        verbose_name_plural = "Подписчики"
+        db_table = "subscriber"
+        ordering = ["-created_at", "email"]
+
+    def __str__(self):
+        return self.email
+
+
+class Message(models.Model):
+    subject = models.CharField(max_length=150, verbose_name="Тема письма")
+    text = models.TextField(verbose_name="Текст письма")
+    subscriber = models.ForeignKey(
+        Subscriber,
+        on_delete=models.SET_NULL,
+        verbose_name="Получатель",
+        null=True,
+        blank=True,
+        related_name="messages"
+    )
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="Дата создания")
+
+    class Meta:
+        verbose_name = "Письмо"
+        verbose_name_plural = "Письма"
+        db_table = "message"
+        ordering = ["-created_at", "subject"]
+
+    def __str__(self):
+        return self.subject
